2e8e049d3ff08caa204ff1eb263368885a6b99bf
Hyde Stefan <79136747+DrHyde01@users.noreply.github.com>
2023-08-17 09:04:01 +0200

    Init server Update .gitignore

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const PORT = 8000
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT} ⚡️`);
+})
